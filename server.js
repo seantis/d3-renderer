82b2ae6f4d8fd87ed2133946382af268d87e2c63
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2017-03-09 09:26:12 +0100

    Add PNG service (PhantomJS only)

--- server.js
+++ server.js
@@ -21,6 +21,17 @@ function renderElementToPdf(page, selector) {
 	return pdf;
 }
 
+function renderElementToPng(page, selector) {
+	var prevClipRect = page.clipRect;
+	page.clipRect = page.evaluate(function(selector) {
+		return document.querySelector(selector).getBoundingClientRect();
+	}, selector);
+    png = page.renderBase64('png');
+	page.clipRect = prevClipRect;
+
+	return png;
+}
+
 
 var app = new Routes();
 
@@ -73,6 +84,36 @@ app.post('/d3/pdf', function(req, res) {
 
         var pdf = renderElementToPdf(page, '#viewport');
         res.send(pdf);
+    } else {
+        res.statusCode = 400;
+        res.send(
+            "Missing parameters. Make sure to send a valid JSON request."
+        );
+    }
+});
+
+app.post('/d3/png', function(req, res) {
+    data = JSON.parse(req.post);
+
+    if (data.scripts && data.main && data.params) {
+        page = webpage.create();
+        page.viewportSize = {width: 1000, height: 1000};
+        page.setContent(
+            '<html><head></head><body><div id="viewport"></div></body></html>',
+            'http://www.nohost.org'
+        );
+        page.injectJs('d3.min.js');
+
+        var scripts = '';
+        for (var i = 0; i < data.scripts.length; i++) {
+            scripts += '\n' + data.scripts[i];
+        }
+        var params = 'var params=JSON.parse(\'' + JSON.stringify(data.params) + '\');';
+        var main = data.main + '(params)(d3.select("#viewport").node());';
+        page.evaluateJavaScript('function(){' + scripts + params + main + '}');
+
+        var png = renderElementToPng(page, '#viewport');
+        res.send(png);
     } else {
         res.statusCode = 400;
         res.send(
